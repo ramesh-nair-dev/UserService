014013e48dba6a2f354d8c00c4301f770b4464a7
Ramesh <ramesh200212@gmail.com>
2025-08-24 15:59:00 +0530

    Implementing oAuth2

---
+++ src/main/java/com/example/userauthservice/config/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.example.userauthservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+//    @Bean
+////    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+////        http
+////                .csrf(csrf -> csrf.disable())
+////                .authorizeHttpRequests((requests) -> requests
+////                        .requestMatchers("/auth/signup","/auth/login","/auth/validate").permitAll()
+////                        .anyRequest().authenticated()
+////                );
+////
+////        return http.build();
+////    }
+
+}
--- src/test/java/com/example/userauthservice/UserAuthServiceApplicationTests.java
+++ src/test/java/com/example/userauthservice/UserAuthServiceApplicationTests.java
@@ -1,13 +1,42 @@
 package com.example.userauthservice;
 
+import com.example.userauthservice.security.service.JpaRegisteredClientRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+
+import java.util.UUID;
 
 @SpringBootTest
 class UserAuthServiceApplicationTests {
 
+    @Autowired
+    private JpaRegisteredClientRepository registeredClientRepository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Test
     void contextLoads() {
+        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+				.clientId("productService")
+				.clientSecret(bCryptPasswordEncoder.encode("password"))
+				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+				.redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
+				.postLogoutRedirectUri("http://127.0.0.1:8080/")
+				.scope(OidcScopes.OPENID)
+				.scope(OidcScopes.PROFILE)
+				.clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+				.build();
+        registeredClientRepository.save(oidcClient);
     }
 
 }
